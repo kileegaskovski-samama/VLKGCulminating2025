29ef6b33b6f946041362d8e532b3757e574d3764
kileegaskovski-samama <kileegaskovski-samama@10.20.1.251>
2025-12-05 13:12:01 -0500

    initial code drop

---
+++ VLKGCulminating/src/Driver.java
@@ -0,0 +1,9 @@
+import java.util.ArrayList;
+
+public class Driver {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		ArrayList<Card> card = new ArrayList<>();
+		System.out.println(card.size());
+	}
+}
