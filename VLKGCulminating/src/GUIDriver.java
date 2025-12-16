fe5067456c98dae9c210c6bdac5c1d4b4bc032d6
kileegaskovski-samama <kileegaskovski-samama@10.20.1.115>
2025-12-16 13:00:59 -0500

    Deleted comments! Too cluttered!

--- VLKGCulminating/src/GUIDriver.java
+++ VLKGCulminating/src/GUIDriver.java
@@ -39,28 +39,17 @@ public class GUIDriver extends Application {
 		
 		
 		Grid playingGrid = new Grid();
-//		playingGrid.print();
-//		playingGrid.addToken(0);
-//		playingGrid.print();
-//		playingGrid.addToken(5);
-//		playingGrid.print();
-//		playingGrid.addToken(5);
-//		playingGrid.print();
-//		playingGrid.addToken(6);
 		playingGrid.print();
 		
 		
-//        root.setAlignment(Pos.CENTER);
 
         GridPane grid = new GridPane();
         grid.setStyle("-fx-background-color: transparent;"); // makes background transparent so can see shapes behind 
-//        grid.setAlignment(Pos.CENTER);
         grid.relocate(30, 70);
         grid.setPrefSize(600, 700); // does nothing?
         grid.setMaxSize(600, 700);
         
         Circle token = new Circle(30);
-//        token.setFill(Color.RED);
         token.setCenterX(31);
         token.setCenterY(50);
         token.setFill(Color.YELLOW);
@@ -93,17 +82,14 @@ public class GUIDriver extends Application {
                 Circle circle = new Circle(30);
                 double rowLocation = playingGrid.getRowVal(r);
                 circle.setCenterY(rowLocation);
-//                System.out.println(r + "Row: " + rowLocation);
                 double columnLocation = playingGrid.getColumnVal(c);
                 circle.setCenterX(columnLocation);
-//                System.out.println(c + "Column: " + columnLocation + "\n");
                 circle.setFill(Color.TRANSPARENT);
                 arrCircles[r][c] = circle;
                 root.getChildren().add(circle);
             }
         }
         
-//        System.out.println(token.getCenterX());
         
         
         Button confirm = new Button("CONFIRM");
@@ -112,83 +98,32 @@ public class GUIDriver extends Application {
         Button nextPlayer = new Button("END TURN");
         nextPlayer.relocate(351, 630);
         
-//        int player = playingGrid.getTurnsNoModify();
-//        
-//        if (player == 1) {
-//        	token.setFill(Color.YELLOW);
-//        }
-//        else {
-//        	token.setFill(Color.RED);
-//        }
         
         scene.setOnKeyPressed(e-> {
         	double move = 80.0;
-        	double temp = 50;
-        	
-//        	int playerColor = playingGrid.getTurnsNoModify();
-        	
+        	double temp = 50;        	
         	
         	token.setCenterY(50);
         	
         	if (e.getCode() == KeyCode.A) {
         		if (turnOver) {
-	//        		token.requestFocus();
-	//        		token.setCenterY(150);
-	//        		System.out.println(token.getCenterY());
-	//        		System.out.println(token.getCenterX());
 	        		token.setCenterX(token.getCenterX() - move);
-//	        		if (playerColor == 1) {
-//	        			token.setFill(Color.YELLOW);
-//	        		}
-//	        		else {
-//	        			token.setFill(Color.RED);
-//	        		}
         		}
         	}
         	if (e.getCode() == KeyCode.D) {
         		if (turnOver) {
 	        		token.setCenterY(temp);
-	//        		System.out.println(token.getCenterX());
 	        		token.setCenterX(token.getCenterX() + move);
-	//        		System.out.println(token.getCenterX());
-	//        		token.setCenterY(token.getCenterY() - token.getCenterY() + 50);
-//	        		if (playerColor == 1) {
-//	        			token.setFill(Color.YELLOW);
-//	        		}
-//	        		else {
-//	        			token.setFill(Color.RED);
-//	        		}
         		}
         	}
-//        	if (e.getCode() == KeyCode.W) {
-//        		token.setCenterY(token.getCenterY() + 1);
-//        		
-//        		int column = playingGrid.getColumnNum(token.getCenterX());
-//            	double rowVal = playingGrid.addToken(column);
-//            	int row = playingGrid.getRowNum(column);
-//        		
-//        		Path path = new Path();
-//            	double x1 = token.getCenterX();
-//            	double y1 = token.getCenterY();
-//            	path.getElements().add(new MoveTo(x1, y1));
-////            	path.getElements().add(new LineTo(x1, rowVal));
-//            	
-//            	PathTransition transition = new PathTransition();
-//            	transition.setNode(token);
-//            	transition.setDuration(Duration.seconds(2));
-//            	transition.setPath(path);
-//            	transition.setCycleCount(1);
-//            	transition.play();
-//        	}
         	
         	
         });
                 
         confirm.setOnAction(e-> {
-        	if (turnOver) {
+        	if (turnOver) { // prevents user from pressing button twice in a row
 	        	int column = playingGrid.getColumnNum(token.getCenterX());
 	        	double rowVal = playingGrid.addToken(column);
-//	        	int row = playingGrid.getRowNum(column);
 	        	System.out.println("Check 1");
 	        	int row = playingGrid.getCurrentRow();
 	        	System.out.println("Check 2");
@@ -211,18 +146,10 @@ public class GUIDriver extends Application {
 	        		if (playerColor == 1) {
 	        			arrCircles[row][column].setFill(Color.RED);
 	        			arrCircles[row][column].setStroke(Color.BLACK);
-	//        			token.setFill(Color.RED);
-	//        			
-	//        			token.relocate(0, 0);
-	//        			token.setCenterY(50);
-	//        			System.out.println(token.getCenterY());
-	//        			System.out.println("X: " + token.getCenterX());
 	        		}
 	        		if (playerColor == 2) {
-	//        			System.out.println("PLAYER: " + playerColor);
 	        			arrCircles[row][column].setFill(Color.YELLOW);
 	        			arrCircles[row][column].setStroke(Color.BLACK);
-	//        			token.setFill(Color.YELLOW);
 	        		}
 	        	});
 	        	
@@ -244,7 +171,6 @@ public class GUIDriver extends Application {
         	double x1 = token.getCenterX();
         	double y1 = token.getCenterY();
         	path.getElements().add(new MoveTo(x1, y1));
-//        	path.getElements().add(new LineTo(x1, 50));
         	path.getElements().add(new LineTo(x1, y1 + 10));
         	
         	PathTransition transition = new PathTransition();
@@ -254,9 +180,6 @@ public class GUIDriver extends Application {
         	transition.setPath(path);
         	transition.play();
         	
-//        	token.setFill(Color.TRANSPARENT);
-        	
-//        	transition.setOnFinished(g-> {
         	if (playerColor == 1) {
     			token.setFill(Color.YELLOW);
     		}
@@ -266,7 +189,6 @@ public class GUIDriver extends Application {
         	
         	turnOver = true;
         	
-//        	});
         });
         
         Button grey = new Button("Grey");
